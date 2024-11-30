a39a1e15684fa24f23f18de39b71e9cb199765eb
albertpang123 <152299980+albertpang123@users.noreply.github.com>
2024-11-30 22:21:13 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
 #include "game.h"
 
+using namespace std;
+
 void displayMenu() {
-    std::cout << R"(
+    cout << R"(
   _____             _          __   _   _           __          __           _       _ _ _ 
  |  __ \           | |        / _| | | | |          \ \        / /          | |     | | | |
  | |  | |_   _  ___| |   ___ | |_  | |_| |__   ___   \ \  /\  / /__  _ __ __| |___  | | | |
@@ -9,47 +11,47 @@ void displayMenu() {
  | |__| | |_| |  __/ | | (_) | |   | |_| | | |  __/    \  /\  / (_) | | | (_| \__ \ |_|_|_|
  |_____/ \__,_|\___|_|  \___/|_|    \__|_| |_|\___|     \/  \/ \___/|_|  \__,_|___/ (_|_|_)
     )";
-    std::cout << "\n1. Single-Player\n2. Multiplayer\n3. Quit\n";
+    cout << "\n1. Single-Player\n2. Multiplayer\n3. Quit\n";
 }
 
 int main() {
     int choice;
     do {
         displayMenu();
-        std::cout << "Choose an option: ";
-        std::cin >> choice;
+        cout << "Choose an option: ";
+        cin >> choice;
 
         if (choice == 1) {
-            HangmanGame game(7, true);
+            DuelGame game(7, true);
             if (game.loadRandomWord("word.txt")) {
                 while (!game.isGameWon() && !game.isGameLost()) {
                     game.displayState();
                     char guess;
-                    std::cout << "\nEnter your guess: ";
-                    std::cin >> guess;
+                    cout << "\nEnter your guess: ";
+                    cin >> guess;
                     game.playTurn(guess);
                 }
                 game.printResult();
             }
         } else if (choice == 2) {
-            HangmanGame game(7, false);
-            std::string word;
-            std::cout << "Player 1, enter a word for Player 2 to guess: ";
-            std::cin >> word;
+            DuelGame game(7, false);
+            string word;
+            cout << "Player 1, enter a word for Player 2 to guess: ";
+            cin >> word;
             game.setSecretWord(word);
             game.clearScreen();
 
             while (!game.isGameWon() && !game.isGameLost()) {
                 game.displayState();
                 char guess;
-                std::cout << "\nPlayer 2, enter your guess: ";
-                std::cin >> guess;
+                cout << "\nPlayer 2, enter your guess: ";
+                cin >> guess;
                 game.playTurn(guess);
             }
             game.printResult();
         }
     } while (choice != 3);
 
-    std::cout << "Thanks for playing Duel of the Words!\n";
+    cout << "Thanks for playing Duel of the Words!\n";
     return 0;
 }
